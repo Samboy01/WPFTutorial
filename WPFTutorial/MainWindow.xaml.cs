ddb58dc38b58899d509b55039ad73f50cf381bb4
samai <samai@DESKTOP-FESCBGL>
2024-11-06 18:02:35 +0100

    Add project files.

---
+++ WPFTutorial/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace WPFTutorial
+{
+    
+
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //Stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                //run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            //Toggle
+            running = !running;
+        }
+    }
+}
