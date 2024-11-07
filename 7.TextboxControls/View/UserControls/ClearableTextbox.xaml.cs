f61b29f09c81f021bea0e33e3dbe0c0e5d9a889c
Sam Airey <s2308793@student.hb.se>
2024-11-07 16:04:42 +0100

    yap

---
+++ 7.TextboxControls/View/UserControls/ClearableTextbox.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _7.TextboxControls.View.UserControls
+{
+
+    public partial class ClearableTextbox : UserControl
+    {
+        public ClearableTextbox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value; 
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            //Gör att vi stannar i textboxen
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Om textboxen är tom så kan vi se placeholdern (Texten)
+            //När vi skriver i textboxen försvinner placholdern (Texten)
+            if(string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ 8.DataBindningsINotify/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace _8.DataBindningsINotify
+{
+    
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            // Sätter datakontexten till detta objekt (MainWindow)
+            // så att UI kan binda till properties i denna klass
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string boundText;
+
+        // Event som triggas när en property ändras
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value;// Uppdaterar värdet
+
+                // Skickar notifiering till UI att BoundText har ändrats
+                OnPropertyChanged();
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
