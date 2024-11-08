14fd9be881fc47882df29dc5b70df2a334dd2e08
Sam Airey <s2308793@student.hb.se>
2024-11-08 13:37:49 +0100

    OpenFileDiaglogs

---
+++ 10.OpenFileDigalogs/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10.OpenFileDigalogs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFire_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            //Anger vilka filer som ska visas
+            fileDialog.InitialDirectory = @"C:\Users\samai\source\repos\WPFTutorial";
+            //fileDialog.Filter = "C# Soruce Files | *.cs";
+            fileDialog.Title = "Please pick a file";
+
+            //Succes lagrar resultatet av valt fil eller ej
+            bool ? success = fileDialog.ShowDialog();
+            if(success == true)
+            {
+                //Visar filträdet
+                string path = fileDialog.FileName;
+                //Visar bara filen
+                string fileName = fileDialog.SafeFileName;
+
+                tbInfo.Text = path;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
