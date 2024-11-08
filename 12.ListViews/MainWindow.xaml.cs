3e9ce2cdf184b62591637809235992a4527553f9
Sam Airey <s2308793@student.hb.se>
2024-11-08 17:30:40 +0100

    List views

---
+++ 12.ListViews/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _12.ListViews
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Add(txtEntry.Text);
+            txtEntry.Clear();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //int index = lvEntries.SelectedIndex;
+            //object item = lvEntries.SelectedItem;
+            var items = lvEntries.SelectedItems;
+            var result = MessageBox.Show($"Are you sure you wan to delete :{items.Count}", "Sure?", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                //Gör en kopia så att vi inte tar bort från listan samtidigt som vi itererar
+                var itemsList = new ArrayList(items);
+                foreach ( var item in itemsList)
+                {
+                    lvEntries.Items.Remove(item);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lvEntries.Items.Clear();
+        }
+    }
+}
