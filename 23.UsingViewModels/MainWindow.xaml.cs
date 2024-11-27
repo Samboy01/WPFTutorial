c7fe0992b211d79da6aa4407de53c58b9782ec64
Sam Airey <s2308793@student.hb.se>
2024-11-27 16:20:51 +0100

    mvvm

---
+++ 23.UsingViewModels/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using _23.UsingViewModels.ViewModel;
+using System.Windows;
+
+namespace _23.UsingViewModels
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindowViewModel vm = new MainWindowViewModel();
+            // Sätter DataContext för fönstret till den skapade ViewModel-instansen
+            DataContext = vm;
+        }
+    }
+}
